a25acd5eb787c7ee75eff1bb9030148ee8c5e69e
Christian Alfonso <alfonso.christian.macatangay@gmail.com>
2024-11-20 23:04:38 +0800

    new login

--- login.php
+++ login.php
@@ -85,7 +85,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
 
             
-            <form action="#">
+            <form method="POST" action="login.php">
 
                 <div class="form-group  mt-4">
                     <label for="role">Role:</label>
@@ -117,13 +117,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="form-group mt-3">
                     <button type="submit" class="btn btn-danger w-100">Login</button>
                 </div>
-
-
-                
-
-                
-                
-                
+  
             </form>
         </div>
     </div>
