f23880f4adceb765ed60d274ab0e825422abb767
Christian Alfonso <alfonso.christian.macatangay@gmail.com>
2024-11-23 01:52:28 +0800

    updated students

--- import_students.php
+++ import_students.php
@@ -49,7 +49,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES['student_file'])) {
 <style>
 
     .admin-import-student {
-        min-width: 1200px;
+        height: 100vh;
+        width: 100vw;
     }
 
     .nav-item {
@@ -74,6 +75,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES['student_file'])) {
     .sidebar {
         width: 300px;
     }
+
+    .burger {
+        display: none;
+    }
+    
+    @media (max-width: 700px) {
+        .sidebar {
+            display: none;
+        }
+
+        .burger {
+            display: block;
+        }
+    }
+    
 </style>
 <body>
 
@@ -138,7 +154,65 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES['student_file'])) {
     </div>
 
     <div class="main-content flex-grow-1 p-3">
-        <h2>Import Students (CSV)</h2>
+    <div class="container-fluid d-flex justify-content-between">
+    <h2>Import Students (CSV)</h2>
+    <button class="navbar-toggler navbar-light burger" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo" aria-controls="demo">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+                <!-- Offcanvas Sidebar -->
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="demo" aria-labelledby="offcanvasDemoLabel">
+                    
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasDemoLabel">Sidebar Menu</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+
+                        <div class="offcanvas-body">
+                            <ul class="nav">
+                                <li class="nav-item">
+                                    <a href="admin_announcements.php">Announcements</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="change_password.php">Change Password</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="offcanvasStudentsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Students
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="offcanvasStudentsDropdown">
+                                        <li><a class="dropdown-item" href="register_student.php">Register Students</a></li>
+                                        <li><a class="dropdown-item" href="import_students.php">Import Students</a></li>
+                                        <li><a class="dropdown-item" href="users_students.php">View Students</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="offcanvasTeachersDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Teachers
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="offcanvasTeachersDropdown">
+                                        <li><a class="dropdown-item" href="register_teacher.php">Register Teachers</a></li>
+                                        <li><a class="dropdown-item" href="import_teachers.php">Import Teachers</a></li>
+                                        <li><a class="dropdown-item" href="users_teachers.php">View Teachers</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="offcanvasParentsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Parents
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="offcanvasParentsDropdown">
+                                        <li><a class="dropdown-item" href="register_parent.php">Register Parents</a></li>
+                                        <li><a class="dropdown-item" href="import_parents.php">Import Parents</a></li>
+                                        <li><a class="dropdown-item" href="users_parents.php">View Parents</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="logout.php">Logout</a>
+                                </li>
+                            </ul>
+                        </div>
+                </div>
+        </div>
 
         <div class="container shadow p-5">
                 <form method="POST" enctype="multipart/form-data" action="import_students.php">
